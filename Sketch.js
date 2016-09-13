0b88cf1f53b7044b464c19d75c3a12fe8d9f4159
Tatyana Lee <Tatyana Lee>
2016-09-13 03:47:28 -0500

    example

--- Sketch.js
+++ Sketch.js
@@ -1,20 +1,17 @@
-var x;
-
-function setup(){
-  createCanvas(150, 300);
-  x = 20;
-  println("my awesome variable=" , x,"width = ")
-  line(10, 10, width-5, height-5);
-  ellipse(10,10, 10);
-  ellipse(10, 10, x);
-  ellipse(width-5, height-5, x);
+function setup() {
+  createCanvas(480, 120);
+  fill(255);
+  stroke(102);
 }
-function draw(){
-  background(150);
 
-  for(varn = 0; n <1000){
-    line(10, 10, 5, 5);
-    ellipse(10, 10, x);
-    ellipse(width-5, height-5, x);
-    }
+function draw() {
+  //lght turquoise
+  background(0);
+  for (var y = 20; y <= height-20; y += 10) {
+    for (var x = 20; x <= width-20; x += 10)
+  {
+    ellipse(x, y, 4, 4);
+    line(x, y, 240, 60);
+  }
+  }
 }
