c196d367fa70444ef218e6b3aee6727932c486a7
Tatyana Lee <Tatyana Lee>
2016-09-13 05:42:46 -0500

    Finished

--- Sketch.js
+++ Sketch.js
@@ -1,18 +1,22 @@
 function setup() {
   createCanvas(500, 500);
-  stroke(102);
 }
 
 function draw() {
   //lght turquoise background(204, 226, 225)
   //dark blue background(0, 26, 51)
-  background(204, 226, 225);
-  for (var y = 20; y <= height-20; y += 10) {
-    for (var x = 20; x <= width-20; x += 10)
+  background (0, 89, 56);
+  for (var y = 10; y <= height-20; y += 10) {
+    for (var x = 10; x <= width-20; x += 10)
   {
-    fill(0, 26, 51);
-    ellipse(x, y, 4, 4);
-    line(x, y, 300, 300);
-  }
+    fill(200, 0, 150);
+    ellipse(x, y, x + 40, y/50);
+
+    fill(0, 0, 150);
+    line(x - y, y/50, x, y);
+
+    fill(255);
+    line(x, y, 250, 250);
+    }
   }
 }
