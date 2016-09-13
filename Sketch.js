7cb8c0ba1347a6687b5805cea23d16bea08bf915
Tatyana Lee <Tatyana Lee>
2016-09-13 04:22:11 -0500

    pins and lines

--- Sketch.js
+++ Sketch.js
@@ -1,17 +1,18 @@
 function setup() {
-  createCanvas(480, 120);
-  fill(255);
+  createCanvas(500, 500);
   stroke(102);
 }
 
 function draw() {
-  //lght turquoise
-  background(0);
+  //lght turquoise background(204, 226, 225)
+  //dark blue background(0, 26, 51)
+  background(204, 226, 225);
   for (var y = 20; y <= height-20; y += 10) {
     for (var x = 20; x <= width-20; x += 10)
   {
+    fill(0, 26, 51);
     ellipse(x, y, 4, 4);
-    line(x, y, 240, 60);
+    line(x, y, 300, 300);
   }
   }
 }
